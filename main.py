8c988a03e2c32502065b498141086c028f85f1e7
kuriimu01 <danya.but.05@gmail.com>
2022-12-02 18:34:48 +0200

    some changes in buttons and now everything become more flexible

--- main.py
+++ main.py
@@ -3,69 +3,133 @@ import tkinter.messagebox
 import customtkinter
 
     
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("System")  # "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("green")  # "blue" (standard), "green", "dark-blue"
 
 class App(customtkinter.CTk):
     """Main app class"""
 
-    #screen config
-    WIDTH = 560
-    HEIGHT = 525
+    # screen config
+    WIDTH = 380
+    HEIGHT = 380
     
     def __init__(self):
         super().__init__()  
         
-        #configure window
-        self.title("Calculator")
+        # configure window
+        self.title("Calculator by kuriimu01")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.grid_columnconfigure(0, weight=1)
         
-        #input
-        self.entry = customtkinter.CTkEntry(self, height=60, width=560, justify=RIGHT, placeholder_text='by kuriimu01',
+        # create main entry
+        self.entry = customtkinter.CTkEntry(self, height=60, justify=RIGHT,
                     font=customtkinter.CTkFont(family='monospace', size=40))
         self.entry.grid(row=0, pady=10, columnspan=4, sticky="nsew")
         
-        #create buttons frame
-        self.bframe = customtkinter.CTkFrame(self,  width=560, corner_radius=0)
-        self.bframe.grid(row=1, pady=10, column=1, rowspan=4, sticky="nsew")
- 
-        #buttons
-        self.b1 = customtkinter.CTkButton(self.bframe, text='%', height=60).grid(row=1, column=0)
-        self.b2 = customtkinter.CTkButton(self.bframe, text='CE', height=60).grid(row=1, column=1)
-        self.b3 = customtkinter.CTkButton(self.bframe, text='C', height=60).grid(row=1, column=2)
-        self.b4 = customtkinter.CTkButton(self.bframe, text='<', height=60).grid(row=1, column=3)
-        self.b5 = customtkinter.CTkButton(self.bframe, text='1/x', height=60).grid(row=2, column=0)
-        self.b6 = customtkinter.CTkButton(self.bframe, text='x²', height=60).grid(row=2, column=1)
-        self.b7 = customtkinter.CTkButton(self.bframe, text='√x', height=60).grid(row=2, column=2)
-        self.b8 = customtkinter.CTkButton(self.bframe, text='÷', height=60).grid(row=2, column=3)
-        self.b9 = customtkinter.CTkButton(self.bframe, text='7', height=60, command=lambda : App.add_num(7)).grid(row=3, column=0)
-        self.b10 = customtkinter.CTkButton(self.bframe, text='8', height=60, command=lambda : App.add_num(8)).grid(row=3, column=1)
-        self.b11 = customtkinter.CTkButton(self.bframe, text='9', height=60, command=lambda : App.add_num(9)).grid(row=3, column=2)
-        self.b12 = customtkinter.CTkButton(self.bframe, text='×', height=60).grid(row=3, column=3)
-        self.b13 = customtkinter.CTkButton(self.bframe, text='4', height=60, command=lambda : App.add_num(4)).grid(row=4, column=0)
-        self.b14 = customtkinter.CTkButton(self.bframe, text='5', height=60, command=lambda : App.add_num(5)).grid(row=4, column=1)
-        self.b15 = customtkinter.CTkButton(self.bframe, text='6', height=60, command=lambda : App.add_num(6)).grid(row=4, column=2)
-        self.b16 = customtkinter.CTkButton(self.bframe, text='-', height=60).grid(row=4, column=3)
-        self.b17 = customtkinter.CTkButton(self.bframe, text='1', height=60, command=lambda : App.add_num(1)).grid(row=5, column=0)
-        self.b18 = customtkinter.CTkButton(self.bframe, text='2', height=60, command=lambda : App.add_num(2)).grid(row=5, column=1)
-        self.b19 = customtkinter.CTkButton(self.bframe, text='3', height=60, command=lambda : App.add_num(3)).grid(row=5, column=2)
-        self.b20 = customtkinter.CTkButton(self.bframe, text='+', height=60).grid(row=5, column=3)
-        self.b21 = customtkinter.CTkButton(self.bframe, text='+/-', height=60).grid(row=6, column=0)
-        self.b22 = customtkinter.CTkButton(self.bframe, text='0', height=60, command=lambda : App.add_num(0)).grid(row=6, column=1)
-        self.b23 = customtkinter.CTkButton(self.bframe, text=',', height=60).grid(row=6, column=2)
-        self.b24 = customtkinter.CTkButton(self.bframe, text='=', height=60).grid(row=6, column=3)
-                   
-        #change appearence 
+        # create buttons frame
+        self.bframe = customtkinter.CTkFrame(self, corner_radius=0)
+        self.bframe.grid(row=1, pady=10, column=0, rowspan=4, sticky="nsew")
+        self.bframe.grid_columnconfigure((0,1,2,3), weight=1)
+        self.bframe.grid_rowconfigure(1, weight=1)
+        # buttons
+        self.b1 = customtkinter.CTkButton(self.bframe, text='%', command=lambda : self.add_num('%'))
+        self.b2 = customtkinter.CTkButton(self.bframe, text='CE')
+        self.b3 = customtkinter.CTkButton(self.bframe, text='C', command= lambda : self.clear())
+        self.b4 = customtkinter.CTkButton(self.bframe, text='<', command= lambda : self.delete_one())
+        self.b5 = customtkinter.CTkButton(self.bframe, text='1/x')
+        self.b6 = customtkinter.CTkButton(self.bframe, text='x²')
+        self.b7 = customtkinter.CTkButton(self.bframe, text='√x')
+        self.b8 = customtkinter.CTkButton(self.bframe, text='÷', command= lambda : self.add_num('÷'))
+        self.b9 = customtkinter.CTkButton(self.bframe, text='7', command=lambda : self.add_num(7))
+        self.b10 = customtkinter.CTkButton(self.bframe, text='8', command=lambda : self.add_num(8))
+        self.b11 = customtkinter.CTkButton(self.bframe, text='9', command=lambda : self.add_num(9))
+        self.b12 = customtkinter.CTkButton(self.bframe, text='×', command=lambda : self.add_num('×'))
+        self.b13 = customtkinter.CTkButton(self.bframe, text='4', command=lambda : self.add_num(4))
+        self.b14 = customtkinter.CTkButton(self.bframe, text='5', command=lambda : self.add_num(5))
+        self.b15 = customtkinter.CTkButton(self.bframe, text='6', command=lambda : self.add_num(6))
+        self.b16 = customtkinter.CTkButton(self.bframe, text='-', command=lambda : self.add_num('-'))
+        self.b17 = customtkinter.CTkButton(self.bframe, text='1', command=lambda : self.add_num(1))
+        self.b18 = customtkinter.CTkButton(self.bframe, text='2', command=lambda : self.add_num(2))
+        self.b19 = customtkinter.CTkButton(self.bframe, text='3', command=lambda : self.add_num(3))
+        self.b20 = customtkinter.CTkButton(self.bframe, text='+', command=lambda : self.add_num('+'))
+        self.b21 = customtkinter.CTkButton(self.bframe, text='+/-')
+        self.b22 = customtkinter.CTkButton(self.bframe, text='0', command=lambda : self.add_num(0))
+        self.b23 = customtkinter.CTkButton(self.bframe, text='.', command=lambda : self.add_num('.'))
+        self.b24 = customtkinter.CTkButton(self.bframe, text='=', command= lambda : self.calc())
+        
+        # add buttons
+        btn_list = [self.b1, self.b2, self.b3, self.b4, self.b5, self.b6, self.b7, self.b8,
+                    self.b9, self.b10, self.b11, self.b12, self.b13, self.b14, self.b15, self.b16,
+                    self.b17, self.b18, self.b19, self.b20, self.b21, self.b22, self.b23, self.b24,]
+        column = 0
+        row=1
+        for button in btn_list: 
+            if column > 3:
+                column = 0
+                row+=1   
+            if row > 6: break
+            button.grid(row=row, column=column, pady=2, padx=2, sticky="nsew") 
+            column+=1    
+    
+        # change appearence 
         self.opframe = customtkinter.CTkFrame(self,  width=560, corner_radius=0)
-        self.opframe.grid(row=7, columnspan=4, sticky="nsew")
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.opframe, height=60, values=["Light", "Dark", "System"],
-                    command=self.change_appearance_mode_event).grid(row=7, columnspan=4, sticky="nsew")
+        self.opframe.grid(row=7, column=0, columnspan=4,  sticky="nsew")
+        self.opframe.grid_columnconfigure((0,1,2,3), weight=1)
+        self.opframe.grid_rowconfigure(7, weight=1)
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.opframe, height=60, values=["System", "Dark", "Light"],
+                    command=self.change_appearance_mode_event).grid(row=7, columnspan=2, sticky="nsew")
+        self.theme_optionemenu = customtkinter.CTkOptionMenu(self.opframe, height=60, values=["1", "2", "3"],).grid(row=7, column=2, columnspan=3, sticky="nsew")
+        
+        # default values
+        self.add_num(0)
+    
     
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
+    
+    # def change_theme_event(self, theme: str):
+    #    customtkinter.set_default_color_theme(theme)
+    
+    # add num to entry
     def add_num(self, num):
-         self.entry.insert(0, num)
+        entry_text = self.entry.get()
+        if entry_text == '0' and type(num)==int:
+            self.entry.delete(0, customtkinter.END)
+            self.entry.insert(0, num)
+        else:
+            value = entry_text + str(num)
+            self.entry.delete(0, customtkinter.END)
+            self.entry.insert(0, value)
+    
+    # clear entry
+    def clear(self):
+        self.entry.delete(0, customtkinter.END)
+        self.entry.insert(0, 0)
+    
+    def delete_one(self):
+        self.entry.delete(len(self.entry.get())-1, customtkinter.END)
+        
+    # main calculator func
+    def calc(self):
+        result=''
+        entry_text=''
+        for op in self.entry.get():
+            if op=='×':
+                entry_text+='*'
+            elif op=='%':
+                entry_text+='/100'
+            elif op=='÷':
+                entry_text+='/'
+            else:
+                entry_text+=op
+        try:        
+            result = eval(entry_text)
+            self.entry.delete(0, customtkinter.END)
+            self.entry.insert(0, result)
+        except ZeroDivisionError:
+            self.clear()
+            tkinter.messagebox.showerror(title='Error', message='Cannot be divided by zero.')
+        
         
 if __name__=='__main__':
     app = App()

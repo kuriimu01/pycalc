7c922814babd2dfd155c4ee59adc96019146e79b
kuriimu01 <danya.but.05@gmail.com>
2022-12-02 04:21:05 +0200

    trying customtkinter, made it look better

--- main.py
+++ main.py
@@ -1,54 +1,72 @@
 from tkinter import *
-import sys
+import tkinter.messagebox
+import customtkinter
 
-root = Tk()
-root.geometry("390x370")
-root.title("Calculator")
+    
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-#Add input
-e = Entry(root, bg='#1C1C1C', width=40, borderwidth=15, fg="white", font=35, ).grid(row=0, columnspan=4, sticky='NSEW')
+class App(customtkinter.CTk):
+    """Main app class"""
 
-
-#Operator buttons
-percent = Button(root, text="%", padx=38, pady=15, bg='#D4D4D2').grid(row=1, column=0)
-clr_this = Button(root, text="CE", padx=38, pady=15, bg='#D4D4D2').grid(row=1, column=1)
-clr_all = Button(root, text="C", padx=38, pady=15, bg='#D4D4D2').grid(row=1, column=2)
-inverse = Button(root, text="1/x", padx=35, pady=15, bg='#D4D4D2').grid(row=2, column=0)
-sqr = Button(root, text="x²", padx=39, pady=15, bg='#D4D4D2').grid(row=2, column=1)
-sqrroot = Button(root, text="√x", padx=37, pady=15, bg='#D4D4D2').grid(row=2, column=2)
-
-#Left sidebar 
-clr_left = Button(root, text="<", padx=37, pady=15, fg='white', bg='#FF9500', activebackground='#E68600').grid(row=1, column=3)
-div = Button(root, text="÷", padx=37, pady=15, fg='white', bg='#FF9500', activebackground='#E68600').grid(row=2, column=3)
-multiple = Button(root, text="×", padx=37, pady=15, fg='white', bg='#FF9500', activebackground='#E68600').grid(row=3, column=3)
-minus = Button(root, text="-", padx=37, pady=15, fg='white', bg='#FF9500', activebackground='#E68600').grid(row=4, column=3)
-plus = Button(root, text="+", padx=37, pady=15, fg='white', bg='#FF9500', activebackground='#E68600').grid(row=5, column=3)
-equals = Button(root, text="=", padx=37, pady=15, fg='white', bg='#FF9500', activebackground='#E68600').grid(row=6, column=3)
-
-#Int buttons
-b7 = Button(root, text="7", padx=40, pady=15,fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=3, column=0)
-b8 = Button(root, text="8", padx=40, pady=15,fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=3, column=1)
-b9 = Button(root, text="9", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=3, column=2)
-b4 = Button(root, text="4", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=4, column=0)
-b5 = Button(root, text="5", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=4, column=1)
-b6 = Button(root, text="6", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=4, column=2)
-b1 = Button(root, text="1", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=5, column=0)
-b2 = Button(root, text="2", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=5, column=1)
-b3 = Button(root, text="3", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=5, column=2)
-b1 = Button(root, text="+/-", padx=34, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=6, column=0)
-b0 = Button(root, text="0", padx=40, pady=15, fg='white', bg='#505050', 
-    activebackground='#5E5E5E', activeforeground='white').grid(row=6, column=1)
-bp = Button(root, text=".", padx=40, pady=15, fg='white', bg='#505050',
-    activebackground='#5E5E5E', activeforeground='white').grid(row=6, column=2)
-
-root.mainloop()
+    #screen config
+    WIDTH = 560
+    HEIGHT = 525
+    
+    def __init__(self):
+        super().__init__()  
+        
+        #configure window
+        self.title("Calculator")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.grid_columnconfigure(0, weight=1)
+        
+        #input
+        self.entry = customtkinter.CTkEntry(self, height=60, width=560, justify=RIGHT, placeholder_text='by kuriimu01',
+                    font=customtkinter.CTkFont(family='monospace', size=40))
+        self.entry.grid(row=0, pady=10, columnspan=4, sticky="nsew")
+        
+        #create buttons frame
+        self.bframe = customtkinter.CTkFrame(self,  width=560, corner_radius=0)
+        self.bframe.grid(row=1, pady=10, column=1, rowspan=4, sticky="nsew")
+ 
+        #buttons
+        self.b1 = customtkinter.CTkButton(self.bframe, text='%', height=60).grid(row=1, column=0)
+        self.b2 = customtkinter.CTkButton(self.bframe, text='CE', height=60).grid(row=1, column=1)
+        self.b3 = customtkinter.CTkButton(self.bframe, text='C', height=60).grid(row=1, column=2)
+        self.b4 = customtkinter.CTkButton(self.bframe, text='<', height=60).grid(row=1, column=3)
+        self.b5 = customtkinter.CTkButton(self.bframe, text='1/x', height=60).grid(row=2, column=0)
+        self.b6 = customtkinter.CTkButton(self.bframe, text='x²', height=60).grid(row=2, column=1)
+        self.b7 = customtkinter.CTkButton(self.bframe, text='√x', height=60).grid(row=2, column=2)
+        self.b8 = customtkinter.CTkButton(self.bframe, text='÷', height=60).grid(row=2, column=3)
+        self.b9 = customtkinter.CTkButton(self.bframe, text='7', height=60, command=lambda : App.add_num(7)).grid(row=3, column=0)
+        self.b10 = customtkinter.CTkButton(self.bframe, text='8', height=60, command=lambda : App.add_num(8)).grid(row=3, column=1)
+        self.b11 = customtkinter.CTkButton(self.bframe, text='9', height=60, command=lambda : App.add_num(9)).grid(row=3, column=2)
+        self.b12 = customtkinter.CTkButton(self.bframe, text='×', height=60).grid(row=3, column=3)
+        self.b13 = customtkinter.CTkButton(self.bframe, text='4', height=60, command=lambda : App.add_num(4)).grid(row=4, column=0)
+        self.b14 = customtkinter.CTkButton(self.bframe, text='5', height=60, command=lambda : App.add_num(5)).grid(row=4, column=1)
+        self.b15 = customtkinter.CTkButton(self.bframe, text='6', height=60, command=lambda : App.add_num(6)).grid(row=4, column=2)
+        self.b16 = customtkinter.CTkButton(self.bframe, text='-', height=60).grid(row=4, column=3)
+        self.b17 = customtkinter.CTkButton(self.bframe, text='1', height=60, command=lambda : App.add_num(1)).grid(row=5, column=0)
+        self.b18 = customtkinter.CTkButton(self.bframe, text='2', height=60, command=lambda : App.add_num(2)).grid(row=5, column=1)
+        self.b19 = customtkinter.CTkButton(self.bframe, text='3', height=60, command=lambda : App.add_num(3)).grid(row=5, column=2)
+        self.b20 = customtkinter.CTkButton(self.bframe, text='+', height=60).grid(row=5, column=3)
+        self.b21 = customtkinter.CTkButton(self.bframe, text='+/-', height=60).grid(row=6, column=0)
+        self.b22 = customtkinter.CTkButton(self.bframe, text='0', height=60, command=lambda : App.add_num(0)).grid(row=6, column=1)
+        self.b23 = customtkinter.CTkButton(self.bframe, text=',', height=60).grid(row=6, column=2)
+        self.b24 = customtkinter.CTkButton(self.bframe, text='=', height=60).grid(row=6, column=3)
+                   
+        #change appearence 
+        self.opframe = customtkinter.CTkFrame(self,  width=560, corner_radius=0)
+        self.opframe.grid(row=7, columnspan=4, sticky="nsew")
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.opframe, height=60, values=["Light", "Dark", "System"],
+                    command=self.change_appearance_mode_event).grid(row=7, columnspan=4, sticky="nsew")
+    
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+    def add_num(self, num):
+         self.entry.insert(0, num)
+        
+if __name__=='__main__':
+    app = App()
+    app.mainloop()

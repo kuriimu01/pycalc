684d7247c84c7d14fdee2b0bdb3130430221d623
kuriimu01 <danya.but.05@gmail.com>
2022-12-08 01:25:29 +0200

    added square function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from tkinter import *
 import tkinter.messagebox
 import customtkinter
-
-    
+import os, sys
+  
 customtkinter.set_appearance_mode("System")  # "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("green")  # "blue" (standard), "green", "dark-blue"
+  # "blue" (standard), "green", "dark-blue"
 
 class App(customtkinter.CTk):
     """Main app class"""
@@ -38,25 +38,25 @@ class App(customtkinter.CTk):
         self.b2 = customtkinter.CTkButton(self.bframe, text='CE')
         self.b3 = customtkinter.CTkButton(self.bframe, text='C', command= lambda : self.clear())
         self.b4 = customtkinter.CTkButton(self.bframe, text='<', command= lambda : self.delete_one())
-        self.b5 = customtkinter.CTkButton(self.bframe, text='1/x')
-        self.b6 = customtkinter.CTkButton(self.bframe, text='x²')
+        self.b5 = customtkinter.CTkButton(self.bframe, text='1/x', command= lambda : self.add_opp('1/'))
+        self.b6 = customtkinter.CTkButton(self.bframe, text='x²', command=lambda : self.add_num('²'))
         self.b7 = customtkinter.CTkButton(self.bframe, text='√x')
-        self.b8 = customtkinter.CTkButton(self.bframe, text='÷', command= lambda : self.add_num('÷'))
+        self.b8 = customtkinter.CTkButton(self.bframe, text='÷', command= lambda : self.add_opp('÷'))
         self.b9 = customtkinter.CTkButton(self.bframe, text='7', command=lambda : self.add_num(7))
         self.b10 = customtkinter.CTkButton(self.bframe, text='8', command=lambda : self.add_num(8))
         self.b11 = customtkinter.CTkButton(self.bframe, text='9', command=lambda : self.add_num(9))
-        self.b12 = customtkinter.CTkButton(self.bframe, text='×', command=lambda : self.add_num('×'))
+        self.b12 = customtkinter.CTkButton(self.bframe, text='×', command=lambda : self.add_opp('×'))
         self.b13 = customtkinter.CTkButton(self.bframe, text='4', command=lambda : self.add_num(4))
         self.b14 = customtkinter.CTkButton(self.bframe, text='5', command=lambda : self.add_num(5))
         self.b15 = customtkinter.CTkButton(self.bframe, text='6', command=lambda : self.add_num(6))
-        self.b16 = customtkinter.CTkButton(self.bframe, text='-', command=lambda : self.add_num('-'))
+        self.b16 = customtkinter.CTkButton(self.bframe, text='-', command=lambda : self.add_opp('-'))
         self.b17 = customtkinter.CTkButton(self.bframe, text='1', command=lambda : self.add_num(1))
         self.b18 = customtkinter.CTkButton(self.bframe, text='2', command=lambda : self.add_num(2))
         self.b19 = customtkinter.CTkButton(self.bframe, text='3', command=lambda : self.add_num(3))
-        self.b20 = customtkinter.CTkButton(self.bframe, text='+', command=lambda : self.add_num('+'))
+        self.b20 = customtkinter.CTkButton(self.bframe, text='+', command=lambda : self.add_opp('+'))
         self.b21 = customtkinter.CTkButton(self.bframe, text='+/-')
         self.b22 = customtkinter.CTkButton(self.bframe, text='0', command=lambda : self.add_num(0))
-        self.b23 = customtkinter.CTkButton(self.bframe, text='.', command=lambda : self.add_num('.'))
+        self.b23 = customtkinter.CTkButton(self.bframe, text='.', command=lambda : self.add_opp('.'))
         self.b24 = customtkinter.CTkButton(self.bframe, text='=', command= lambda : self.calc())
         
         # add buttons
@@ -79,20 +79,27 @@ class App(customtkinter.CTk):
         self.opframe.grid_columnconfigure((0,1,2,3), weight=1)
         self.opframe.grid_rowconfigure(7, weight=1)
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.opframe, height=60, values=["System", "Dark", "Light"],
-                    command=self.change_appearance_mode_event).grid(row=7, columnspan=2, sticky="nsew")
-        self.theme_optionemenu = customtkinter.CTkOptionMenu(self.opframe, height=60, values=["1", "2", "3"],).grid(row=7, column=2, columnspan=3, sticky="nsew")
-        
+                    command=self.change_appearance_mode_event).grid(row=7, columnspan=4, sticky="nsew")
+               
         # default values
         self.add_num(0)
-    
-    
+
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
-    
-    # def change_theme_event(self, theme: str):
-    #    customtkinter.set_default_color_theme(theme)
-    
+
     # add num to entry
+    def add_opp(self, opp):
+        entry_text = self.entry.get()
+        if entry_text[-1] in '%+-÷×/.':
+            entry_text = entry_text[:-1]
+        elif '+' in entry_text or '-' in entry_text or '×' in entry_text or '÷' in entry_text or '/' in entry_text:
+            self.calc()
+            entry_text = self.entry.get()
+        if entry_text == '0' and opp=='1/':
+            entry_text = entry_text[:-1]
+        self.entry.delete(0, customtkinter.END)
+        self.entry.insert(0, entry_text+opp)        
+
     def add_num(self, num):
         entry_text = self.entry.get()
         if entry_text == '0' and type(num)==int:
@@ -122,6 +129,28 @@ class App(customtkinter.CTk):
                 entry_text+='/100'
             elif op=='÷':
                 entry_text+='/'
+            elif op=='²':
+                range = 0
+                nums=''
+                delete_list = []
+                for op in entry_text:
+                    try:
+                        int(op)
+                    except:
+                        range = entry_text.index(op)
+                if range==0:
+                    count=0
+                    for num in entry_text:
+                        nums+=num
+                    entry_text = entry_text.replace(nums, "")
+                else:
+                    count = 0
+                    for num in entry_text:
+                        if count>range:
+                            nums+=num
+                        count+=1
+                    entry_text = entry_text.replace(nums, "")
+                entry_text+= f'pow({nums} ,2)'
             else:
                 entry_text+=op
         try:        
@@ -131,7 +160,7 @@ class App(customtkinter.CTk):
         except ZeroDivisionError:
             self.clear()
             tkinter.messagebox.showerror(title='Error', message='Cannot be divided by zero.')
-        
+     
         
 if __name__=='__main__':
     app = App()
